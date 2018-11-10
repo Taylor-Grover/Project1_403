5104218dfd645d71eb5134254bf230364e6a22c4
Justin <jschwendy95@gmail.com>
2018-11-10 13:49:52 -0700

    added views and models for admin

---
+++ Project1403/Controllers/AdminController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project1403.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string username, string password)
+        {
+            if(ModelState.IsValid)
+            {
+                return View("Admin");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Project()
+        {
+            return View();
+        }
+
+        public ActionResult SubContractor()
+        {
+            return View();
+        }
+    }
+}
